dba26df31639ea7230ba7c4e1bb3c428745ddba7
victoriliya <victorbiliya@gmail.com>
2021-05-30 15:13:53 +0100

    created change password view forgot password view

---
+++ ELibrary.MVC/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELibrary.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
